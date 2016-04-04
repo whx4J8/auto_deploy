0a8ed923a3e51f004841be0e6e26e63504203ffd
hongxingwang <hongxingwang@sohu-inc.com>
2016-04-04 11:21:05 +0800

    add get server public net ip

--- http/CallbackHandler.py
+++ http/CallbackHandler.py
@@ -1,8 +1,10 @@
 import os
 from bottle import run,route
+import Server
+
 @route("/callback")
 def callbackHandler():
     os.system('./deploy.sh')
     return "ok"
 
-run( host='localhost',port=8082, debug=False)
+run( host=Server.gethostbyname(),port=8082, debug=False)
--- http/Server.py
+++ http/Server.py
@@ -2,18 +2,24 @@ import socket
 import fcntl
 import struct
 
+
 def gethostbyname():
     return socket.gethostbyname(socket.gethostname())
 
+
 def getipaddress(ifname):
-    s = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+    """
+
+    :rtype: object the public net address
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
         s.fileno(),
         0x8915,  # SIOCGIFADDR
         struct.pack('256s', ifname[:15])
     )[20:24])
 
+
 if __name__ == '__main__':
     print  getipaddress('lo')
     print  getipaddress('eth0')
-
